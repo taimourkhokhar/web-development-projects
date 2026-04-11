ef66a00c1b293382e27796564cf1b0400b75929d
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-04-11 20:07:27 +0500

    update

---
+++ backend-projects/TaskManager Rest Api/routes/taskRoutes.js
@@ -0,0 +1,16 @@
+// const express=require('express')
+
+// const router=express.router()
+
+// router.get('/',getTask)
+
+// router.get('/:id',getOneTask)
+// router.post('/',createTask)
+
+// router.delete('/:id',deleteTask)
+
+// router.patch('/:id',updateTask)
+
+
+// module.exports=router
+
--- backend-projects/TaskManager Rest Api/server.js
+++ backend-projects/TaskManager Rest Api/server.js
@@ -1,12 +1,42 @@
 const express=require('express')
-
+const{default:mongoose}=require('mongoose')
 const app=express()
+const path = require('path');
+// Point to the config folder
+require('dotenv').config({ path: path.resolve(__dirname, './config/.env') });
+db_link=process.env.db_link
+console.log(db_link)
 app.use('/',(req,res)=>{
   res.send("Hello server is running")
 })
+// URL: http://localhost:3000/users/123
+// app.get('/users/:userId', (req, res) => {
+//   const id = req.params.userId; // Returns "123"
+//   res.send(`User ID: ${id}`);
+// });
+
+// app.get('/taimour/:userId',(req,res)=>{
+//   const id=req.params.userId;
+//   res.send(`User id is ${id}`)
+// })
+
+
+// app.get('/taimour',(req,res)=>{
+//   const{term,page}=req.query
+//   res.send(`term and page ${term} and ${page}`)
+// })
 
 const PORT=3000
 
-app.listen(PORT,()=>{
+mongoose.connect(db_link).then(()=>{
+  console.log("Database connected successfully")
+  app.listen(PORT,()=>{
   console.log(`Server is running at http://localhost:${PORT}`)
 })
+}).catch((err)=>{
+  console.log(`error while connected to database`,err.message)
+})
+
+
+
+
