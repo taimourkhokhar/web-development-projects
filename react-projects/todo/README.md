e6c6a9fe518f9f98469e355976afe4f5b9619311
Taimour Khokhar <taimouriftikhar2@gmail.com>
2026-05-23 17:20:53 +0500

    Reach todo app

---
+++ react-projects/todo/README.md
@@ -0,0 +1,9 @@
+# React To-Do List
+
+A simple React To-Do app with:
+- Add task
+- Delete task
+- Move task up/down
+- Responsive UI
+
+Built using React and useState hook.
---
+++ react-projects/todo/todoApp/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ToDoList from './toDoList'
+const App = () => {
+  return (
+    <div>
+   <ToDoList/>      
+    </div>
+  )
+}
+
+export default App
